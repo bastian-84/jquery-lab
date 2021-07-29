b86b2eed9885f7db3d909d0fe627ea39a800d8d7
Sebastian Reichenbach <reichenbach.s@gmail.com>
2021-07-29 14:19:53 -0700

    seb: next: jQuery Ancestors

---
+++ js/effects.js
@@ -0,0 +1,294 @@
+$(document).ready(function () {
+    // jQuery methods go here...
+
+
+    // ~~~~~~~~~~~~~~ show hide ~~~~~~~~~~~~~~ //
+    /* 
+    $('#hide').click(function(){
+        $('p').hide();
+    });
+    
+    $('#show').click(function(){
+        $('p').show();
+    });
+ */
+   // ~~~~~~~~~~~~~~ fadeIn( ) ~~~~~~~~~~~~~~ //
+   /* 
+    $('button').click(function(){
+        $('#div1').fadeIn();
+        $('#div2').fadeIn('slow');
+        $('#div3').fadeIn(3000);
+    })
+     */
+
+    
+   // ~~~~~~~~~~~~~~ slideDown( ) ~~~~~~~~~~~~~~ //
+   /* 
+    $('#flip').click(function(){
+        $('#panel').slideDown('slow');
+    })
+      */
+
+ // ~~~~~~~~~~~~~~ slideDown( ) stop( ) ~~~~~~~~~~~~~~ //
+   /* 
+        $('#flip').click(function(){
+            $('#panel').slideDown(5000);
+        });
+        $('#stop').click(function(){
+            $('#panel').stop();
+        })
+     */
+     
+    
+   // ~~~~~~~~~~~~~~ animate( ) ~~~~~~~~~~~~~~ //
+   /* 
+    $('button').click(function(){
+        $('div').animate({left: '250px'})
+    })
+      */
+
+
+    
+  
+
+
+    
+   // ~~~~~~~~~~~~~~ another callback example ~~~~~~~~~~~~~~ //
+   /* 
+    $('button').click(function(){
+        $('p').hide(('slow', function(){
+            alert("alert")
+        }))
+    })
+      */
+
+
+    
+   // ~~~~~~~~~~~~~~ get ~~~~~~~~~~~~~~ //
+   /* 
+    $('#btn1').click(function(){
+        alert('Text: ' + $('#test').text())
+    })
+    
+    $('#btn2').click(function(){
+        alert('HTML: ' + $('#test').html())
+    })
+     */
+
+
+    
+   // ~~~~~~~~~~~~~~ set ~~~~~~~~~~~~~~ //
+   /* 
+    $('#btn1').click(function(){
+        $('#test1').text('test-text...')
+    })
+     
+    $('#btn2').click(function(){
+        $('#test2').html('<b>html output</b>')
+    })
+     
+    $('#btn3').click(function(){
+        $('#test3').val('val...')
+    })
+      */
+
+
+    
+   // ~~~~~~~~~~~~~~ add ~~~~~~~~~~~~~~ //
+   /* 
+    $('#btn1').click(function(){
+        $('p').append('<b>appended text</b>')
+    })
+   
+    $('#btn2').click(function(){
+        $('ol').append('<li>appended li</li>')
+    })
+    */
+
+
+    
+   // ~~~~~~~~~~~~~~ remove ~~~~~~~~~~~~~~ //
+    /* 
+    $('button').click(function(){
+        $('div').remove();
+    })
+      */
+
+
+    
+   // ~~~~~~~~~~~~~~ width( ) height( ) ~~~~~~~~~~~~~~ //
+   
+    $('button').click(function(){
+        var sTxt = '';
+        sTxt += "Width of div: " + $("#div1").width() + "</br>";
+        sTxt += "Height of div: " + $("#div1").height();
+        $('#div1').html(sTxt);
+    })
+     
+
+
+    
+   // ~~~~~~~~~~~~~~ xxxx ~~~~~~~~~~~~~~ //
+   /* 
+    $('button').click(function(){
+        
+    })
+     */
+
+
+    
+   // ~~~~~~~~~~~~~~ xxxx ~~~~~~~~~~~~~~ //
+   /* 
+    $('button').click(function(){
+        
+    })
+     */
+
+
+    
+   // ~~~~~~~~~~~~~~ xxxx ~~~~~~~~~~~~~~ //
+   /* 
+    $('button').click(function(){
+        
+    })
+     */
+
+
+    
+   // ~~~~~~~~~~~~~~ xxxx ~~~~~~~~~~~~~~ //
+   /* 
+    $('button').click(function(){
+        
+    })
+     */
+
+
+    
+   // ~~~~~~~~~~~~~~ xxxx ~~~~~~~~~~~~~~ //
+   /* 
+    $('button').click(function(){
+        
+    })
+     */
+
+
+    
+   // ~~~~~~~~~~~~~~ xxxx ~~~~~~~~~~~~~~ //
+   /* 
+    $('button').click(function(){
+        
+    })
+     */
+
+
+    
+   // ~~~~~~~~~~~~~~ xxxx ~~~~~~~~~~~~~~ //
+   /* 
+    $('button').click(function(){
+        
+    })
+     */
+
+
+    
+   // ~~~~~~~~~~~~~~ xxxx ~~~~~~~~~~~~~~ //
+   /* 
+    $('button').click(function(){
+        
+    })
+     */
+
+
+    
+   // ~~~~~~~~~~~~~~ xxxx ~~~~~~~~~~~~~~ //
+   /* 
+    $('button').click(function(){
+        
+    })
+     */
+
+
+    
+   // ~~~~~~~~~~~~~~ xxxx ~~~~~~~~~~~~~~ //
+   /* 
+    $('button').click(function(){
+        
+    })
+     */
+
+
+    
+   // ~~~~~~~~~~~~~~ xxxx ~~~~~~~~~~~~~~ //
+   /* 
+    $('button').click(function(){
+        
+    })
+     */
+
+
+    
+   // ~~~~~~~~~~~~~~ xxxx ~~~~~~~~~~~~~~ //
+   /* 
+    $('button').click(function(){
+        
+    })
+     */
+
+
+    
+   // ~~~~~~~~~~~~~~ xxxx ~~~~~~~~~~~~~~ //
+   /* 
+    $('button').click(function(){
+        
+    })
+     */
+
+
+    
+   // ~~~~~~~~~~~~~~ xxxx ~~~~~~~~~~~~~~ //
+   /* 
+    $('button').click(function(){
+        
+    })
+     */
+
+
+    
+   // ~~~~~~~~~~~~~~ xxxx ~~~~~~~~~~~~~~ //
+   /* 
+    $('button').click(function(){
+        
+    })
+     */
+
+
+    
+   // ~~~~~~~~~~~~~~ xxxx ~~~~~~~~~~~~~~ //
+   /* 
+    $('button').click(function(){
+        
+    })
+     */
+
+
+    
+   // ~~~~~~~~~~~~~~ xxxx ~~~~~~~~~~~~~~ //
+   /* 
+    $('button').click(function(){
+        
+    })
+     */
+
+
+    
+   // ~~~~~~~~~~~~~~ xxxx ~~~~~~~~~~~~~~ //
+   /* 
+    $('button').click(function(){
+        
+    })
+     */
+
+
+
+
+}); // ./ready fn
